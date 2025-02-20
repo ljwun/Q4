ee5168c890c33caddd34d21eef736df8a1c86a29
ljwun <ljwun@yyuk-dev.link>
2025-02-20 16:16:46 +0800

    refactor: Add interfaces for package SSE to improve testing

--- adapters/sse/interfaces.go
+++ adapters/sse/interfaces.go
@@ -1,3 +1,5 @@
+//go:generate mockgen -package=sse -destination=mock_test.go -source=interfaces.go
+
 package sse
 
 // PublishRequest 表示一個發布請求，包含頻道名稱和訊息。
@@ -6,9 +8,22 @@ type PublishRequest[T any] struct {
 	Message T      `json:"message"`
 }
 
-// ConnectionManager 定義了 SSE 連線管理員的接口，
-// 包含訂閱、取消訂閱、發布訊息和完成的方法。
-type ConnectionManager[T any] interface {
+// IChannel 定義了 SSE 頻道的介面
+type IChannel[T any] interface {
+	// Subscribe 建立一個新的訂閱並返回接收訊息的通道
+	Subscribe() <-chan T
+	// Unsubscribe 取消指定通道的訂閱
+	Unsubscribe(ch <-chan T)
+	// UnsubscribeAll 取消所有訂閱
+	UnsubscribeAll()
+	// Broadcast 將訊息廣播給所有訂閱者
+	Broadcast(message T)
+	// IsIdle 檢查是否沒有訂閱者
+	IsIdle() bool
+}
+
+// IConnectionManager 定義了 SSE 連線管理員的介面
+type IConnectionManager[T any] interface {
 	// Start 啟動 ConnectionManager，開始處理訊息的接收與廣播。
 	// 應在呼叫其他方法前先呼叫此方法。
 	Start()
