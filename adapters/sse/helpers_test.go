8ef86cd39ec18c12e9825f56007f9e4f44aa5313
ljwun <ljwun@yyuk-dev.link>
2025-02-20 18:47:05 +0800

    test: Redirect log output to io.Discard in SSE helpers test

--- adapters/sse/helpers_test.go
+++ adapters/sse/helpers_test.go
@@ -1,5 +1,15 @@
 package sse_test
 
+import (
+	"io"
+	"log"
+)
+
+func init() {
+	// 將日誌輸出重定向到io.Discard
+	log.SetOutput(io.Discard)
+}
+
 // Message 表示一個 SSE 訊息，包含資料字段。
 type Message struct {
 	Data string `json:"data"`
