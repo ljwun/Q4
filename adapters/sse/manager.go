c7f3f0e2968a96350172b0e740e6da2f3ab85976
ljwun <ljwun@yyuk-dev.link>
2025-02-20 16:17:05 +0800

    refactor: Enhance ConnectionManager with Subscriber and Publisher interfaces for improved flexibility

--- adapters/sse/manager.go
+++ adapters/sse/manager.go
@@ -2,21 +2,37 @@ package sse
 
 import (
 	"context"
+	"errors"
 	"log/slog"
 	"sync"
 )
 
+var (
+	// ErrSubscriberRequired 表示建立連線管理器時未提供Subscriber
+	ErrSubscriberRequired = errors.New("subscriber is required")
+	// ErrPublisherNotConfigured 表示嘗試發布訊息時，Publisher未被設定
+	ErrPublisherNotConfigured = errors.New("publisher not configured")
+)
+
+// Subscriber 定義訊息訂閱的介面
+// T 為訊息的資料型別
 type Subscriber[T any] interface {
+	// Subscribe 開始訂閱訊息
+	// 返回一個用於接收訊息的唯讀通道
 	Subscribe() <-chan T
 }
 
+// Publisher 定義訊息發布的介面
+// T 為訊息的資料型別
 type Publisher[T any] interface {
+	// Publish 發布一則訊息
+	// 返回發布過程中可能發生的錯誤
 	Publish(data T) error
 }
 
-// connectionManager 管理多個 SSE 頻道的訂閱與發布。
-// 透過 Redis Stream 實現跨節點的訊息廣播，讓多個服務實例能夠協同運作。
-type connectionManager[T any] struct {
+// ConnectionManager 管理多個 SSE 頻道的訂閱與發布
+// 可透過上游的訊息佇列或其他機制接收訊息，並分發到不同的頻道，實現跨伺服器的訊息廣播
+type ConnectionManager[T any] struct {
 	cancel context.CancelFunc
 	logger *slog.Logger
 
@@ -24,29 +40,75 @@ type connectionManager[T any] struct {
 	wg     sync.WaitGroup // 用於等待所有 goroutine 完成
 	active bool           // 標記 manager 是否正在運作中
 
-	channels   map[string]*Channel[T]        // 儲存所有活躍的頻道
+	channels   map[string]IChannel[T]        // 儲存所有活躍的頻道
 	subscriber Subscriber[PublishRequest[T]] // 用於訂閱上游的訊息
 	publisher  Publisher[PublishRequest[T]]  // 用於發送訊息到上游
 }
 
-// NewConnectionManager 建立一個新的連線管理器
-func NewConnectionManager[T any](sub Subscriber[PublishRequest[T]], pub Publisher[PublishRequest[T]], logger *slog.Logger) ConnectionManager[T] {
-	if logger == nil {
-		logger = slog.Default()
-	}
+// connectionManagerOptions 定義連線管理器的設定選項
+type connectionManagerOptions[T any] struct {
+	subscriber Subscriber[PublishRequest[T]] // 必要：用於接收上游訊息的訂閱者
+	publisher  Publisher[PublishRequest[T]]  // 選用：用於發送訊息到上游的發布者
+	logger     *slog.Logger                  // 選用：用於記錄日誌的 logger
+}
 
-	return &connectionManager[T]{
-		logger:     logger.With("Caller", "ConnectionManager"),
-		channels:   make(map[string]*Channel[T]),
-		subscriber: sub,
-		publisher:  pub,
-		active:     true,
+// ConnectionManagerOption 定義設定選項的函式型別
+type ConnectionManagerOption[T any] func(*connectionManagerOptions[T])
+
+// WithSubscriber 設定連線管理器的訂閱者
+// sub: 實作 Subscriber 介面的訂閱者實例
+func WithSubscriber[T any](sub Subscriber[PublishRequest[T]]) ConnectionManagerOption[T] {
+	return func(o *connectionManagerOptions[T]) {
+		o.subscriber = sub
 	}
 }
 
-// Start 啟動連線管理器，開始處理訊息的接收與廣播。
-// 應在呼叫其他方法前先呼叫此方法。
-func (cm *connectionManager[T]) Start() {
+// WithPublisher 設定連線管理器的發布者
+// pub: 實作 Publisher 介面的發布者實例
+func WithPublisher[T any](pub Publisher[PublishRequest[T]]) ConnectionManagerOption[T] {
+	return func(o *connectionManagerOptions[T]) {
+		o.publisher = pub
+	}
+}
+
+// WithLogger 設定連線管理器的日誌器
+// logger: 用於記錄系統事件的 logger，若未設定則使用預設的 logger
+func WithLogger[T any](logger *slog.Logger) ConnectionManagerOption[T] {
+	return func(o *connectionManagerOptions[T]) {
+		o.logger = logger
+	}
+}
+
+// NewConnectionManager 建立一個新的連線管理器
+// opts: 可變參數，用於設定連線管理器的選項
+// 返回:
+//   - ConnectionManager: 連線管理器實例
+//   - error: 若未提供必要的訂閱者，將返回 ErrSubscriberRequired
+func NewConnectionManager[T any](opts ...ConnectionManagerOption[T]) (IConnectionManager[T], error) {
+	options := connectionManagerOptions[T]{
+		logger: slog.Default(),
+	}
+
+	for _, opt := range opts {
+		opt(&options)
+	}
+
+	if options.subscriber == nil {
+		return nil, ErrSubscriberRequired
+	}
+
+	return &ConnectionManager[T]{
+		logger:     options.logger.With("Caller", "ConnectionManager"),
+		channels:   make(map[string]IChannel[T]),
+		subscriber: options.subscriber,
+		publisher:  options.publisher,
+		active:     true,
+	}, nil
+}
+
+// Start 啟動連線管理器
+// 開始監聽並處理上游的訊息，須在呼叫其他方法前先呼叫此方法
+func (cm *ConnectionManager[T]) Start() {
 	cm.mu.Lock()
 	defer cm.mu.Unlock()
 	if cm.active {
@@ -77,8 +139,9 @@ func (cm *connectionManager[T]) Start() {
 	}()
 }
 
-// Done 停止連線管理器的運作。
-func (cm *connectionManager[T]) Done() {
+// Done 停止連線管理器的運作
+// 會清理所有活躍的頻道並停止訊息處理
+func (cm *ConnectionManager[T]) Done() {
 	cm.mu.Lock()
 	defer cm.mu.Unlock()
 
@@ -95,10 +158,14 @@ func (cm *connectionManager[T]) Done() {
 	clear(cm.channels)
 }
 
-// Subscribe 訂閱指定的頻道。
-// channelName: 要訂閱的頻道名稱
-// 返回: 用於接收訊息的唯讀通道，以及可能的錯誤
-func (cm *connectionManager[T]) Subscribe(channelName string) (<-chan T, error) {
+// Subscribe 訂閱指定的頻道
+// 參數:
+//   - channelName: 要訂閱的頻道名稱
+//
+// 返回:
+//   - <-chan T: 用於接收訊息的唯讀通道
+//   - error: 若管理器已停止運作，將返回 context.Canceled
+func (cm *ConnectionManager[T]) Subscribe(channelName string) (<-chan T, error) {
 	cm.mu.Lock()
 	defer cm.mu.Unlock()
 
@@ -114,15 +181,25 @@ func (cm *connectionManager[T]) Subscribe(channelName string) (<-chan T, error) 
 	return c.Subscribe(), nil
 }
 
-// Publish 發布訊息到指定的頻道。
-// channelName: 目標頻道名稱
-// data: 要發布的訊息內容
-func (cm *connectionManager[T]) Publish(channelName string, data T) error {
+// Publish 發布訊息到指定的頻道
+// 參數:
+//   - channelName: 目標頻道名稱
+//   - data: 要發布的訊息內容
+//
+// 返回:
+//   - error: 可能的錯誤包含：
+//   - context.Canceled: 管理器已停止運作
+//   - ErrPublisherNotConfigured: 未設定Publisher
+func (cm *ConnectionManager[T]) Publish(channelName string, data T) error {
 	cm.mu.RLock()
 	defer cm.mu.RUnlock()
 
 	if !cm.active {
 		return context.Canceled
+	}
+
+	if cm.publisher == nil {
+		return ErrPublisherNotConfigured
 	}
 
 	return cm.publisher.Publish(PublishRequest[T]{
@@ -131,8 +208,12 @@ func (cm *connectionManager[T]) Publish(channelName string, data T) error {
 	})
 }
 
-// Unsubscribe 取消訂閱指定的頻道。
-func (cm *connectionManager[T]) Unsubscribe(channelName string, ch <-chan T) {
+// Unsubscribe 取消訂閱指定的頻道
+// 若該頻道已無訂閱者，會自動移除該頻道
+// 參數:
+//   - channelName: 要取消訂閱的頻道名稱
+//   - ch: 訂閱時取得的訊息通道
+func (cm *ConnectionManager[T]) Unsubscribe(channelName string, ch <-chan T) {
 	cm.mu.Lock()
 	defer cm.mu.Unlock()
 
